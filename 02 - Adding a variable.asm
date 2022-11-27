015d61090d29e60c0aa7864ff460e62e771ecc9b
Moradi1412 <moradi.javid@gmail.com>
2022-11-27 10:54:59 -0800

    organize

--- 02 - Adding a variable.asm
+++ 02 - Adding a variable.asm
@@ -1,3 +1,9 @@
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+
 .data							; this is the data area 
 	sum DWORD 0					; create a variable named sum 
 	
@@ -5,8 +11,8 @@
 	main PROC 
 		mov eax, 5				; move 5 to the eax resgister 
 		add eax, 6				; add 6 to the eax resgister
-		mov sum, eax 
+		mov sum, eax            ; eax = sum
 		
 		INVOKE ExistProcess,0	; end the program 
 	main ENDP
-		
+END main
