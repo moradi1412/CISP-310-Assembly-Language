e533711c81502665893eb87a25a4d55957377113
Moradi1412 <moradi.javid@gmail.com>
2022-11-26 19:07:51 -0800

    01 add two integers, ready to run

--- 01 - Adding two integers.asm
+++ 01 - Adding two integers.asm
@@ -1,6 +1,18 @@
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+.data
+    ; declare variables here
+
+
+.code
 main PROC
 	mov eax,5                 ; move 5 to the eax register / eax = decimal 5 
 	add eax,6                 ; add 6 to the eax register =
 
 	INVOKE ExitProcess,0      ; end the program
 main ENDP
+END main
